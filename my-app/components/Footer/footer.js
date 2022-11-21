2c7de20d79e005b00851a5d992a0513d0b4fde62
Milez42 <111839224+Milez42@users.noreply.github.com>
2022-11-21 22:02:26 +0800

    modified:   my-app/components/Footer/footer.js 	new file:   my-app/components/Launched_projects/projects.js 	modified:   my-app/package-lock.json 	deleted:    my-app/pages/project_page.css 	deleted:    my-app/pages/projectpage.jsx

--- my-app/components/Footer/footer.js
+++ my-app/components/Footer/footer.js
@@ -10,9 +10,9 @@ function Footer() {
             {/* <Link to='/About-Us'>About Us</Link>
             <Link to='/Blog'>Blog</Link>
             <Link to='/Stats'>Stats</Link> */}
-            <a>About Us</a>  
-            <a>Blog</a>  
-            <a>Stats</a>  
+            <a class="classf">About Us</a>  
+            <a class="classf">Blog</a>  
+            <a class="classf">Stats</a>  
             
           </div>
           <div class='footer-link-items'>
@@ -20,9 +20,9 @@ function Footer() {
             {/* <Link to='/Help-and-Support'>Help and Support</Link>
             <Link to='/Forward-Funds'>Forward Funds</Link>
             <Link to='/Our-Rules'>Our Rules</Link> */}
-            <a>Help and Support</a>  
-            <a>Forward Funds</a>  
-            <a>Our Rules</a>  
+            <a class="classf">Help and Support</a>  
+            <a class="classf">Forward Funds</a>  
+            <a class="classf">Our Rules</a>  
            
           </div>
         </div>
@@ -32,9 +32,9 @@ function Footer() {
             {/* <Link to='/Technology'>Technology</Link>
             <Link to='/Creative'>Creative</Link>
             <Link to='/Community'>Community</Link> */}           
-            <a>Technology</a>  
-            <a>Creative</a>  
-            <a>Community</a>  
+            <a class="classf">Technology</a>  
+            <a class="classf">Creative</a>  
+            <a class="classf">Community</a>  
           </div>
         </div>
       </div>
