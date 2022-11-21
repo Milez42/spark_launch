6fd95260cba2ed2bbee07224eeb809c9f42e9558
Milez42 <111839224+Milez42@users.noreply.github.com>
2022-11-21 22:00:37 +0800

    modified:   my-app/components/Launched_projects/launched.js 	new file:   my-app/pages/projectpage/Bionic_Hand.jsx 	new file:   my-app/pages/projectpage/Mini_Pupper.jsx 	new file:   my-app/pages/projectpage/PuffPals.jsx 	new file:   my-app/pages/projectpage/Random_Cruise_Ship.jsx 	new file:   my-app/pages/projectpage/Refund_High_School.jsx 	modified:   my-app/styles/globals.css

--- my-app/components/Launched_projects/launched.js
+++ my-app/components/Launched_projects/launched.js
@@ -4,9 +4,9 @@ import img2 from '../../images/cruise.jpg';
 import img3 from '../../images/bionichand.jpg';
 import img4 from '../../images/islandskies.jpg'; 
 import img5 from '../../images/minipupper.jpg';
-// import img5 from '../../images/minipupper.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
+//import { projects } from './projects';
 
 function CardItem(props) {
   return (
@@ -27,61 +27,64 @@ function CardItem(props) {
   )
 }
 
+
+
 function Launched() {
 
   return (
 
     <div className='cards mt-[3rem]'>
-        <h3 className='flex mx-[5rem]'>Launched Projects</h3>
-        <div className='cards__container'>
-            <div className='cards__wrapper w-screen'>
+        <h4 className='flex mx-[5rem]'>Launched Projects</h4>
+            
+            <div className='cards__container'>
+              <div className='cards__wrapper w-screen'>
                 <ul className='flex w-full px-[5rem] mb-[2rem]'>
                     <CardItem 
                     src={img1}
                     name="Refund High School"
-                    text="Find out what happens to our heroine in her otherworldly experience on a school in the afterlife" 
-                    label="Creative" 
-                    path="/projectpage"
-                    funded="100"/>
+                    text="Find out what happens to our heroine in her otherworldly experience on a school in the afterlife"
+                    label="Creative"
+                    path="/projectpage/Refund_High_School/"
+                    funded= "100"/>
 
                     <CardItem 
                     src={img2}
                     name="Random Cruise Ship"
-                    text="Travel in a Private Cruise!" 
-                    label="Community" 
-                    path="/projectpage"
-                    funded="100"/>
+                    text="Travel in a Private Cruise!"
+                    label="Community"
+                    path="/projectpage/Random_Cruise_Ship/"
+                    funded= "100"/>
                 </ul>
 
                 <ul className='flex w-full px-[5rem]'>
                     <CardItem 
                     src={img3}
                     name="Bionic Hand"
-                    text="Giving a 'hand' to the amputees and the born hand-less" 
-                    label="Technology" 
-                    path="/projectpage"
-                    funded="100"/>
+                    text="Giving a 'hand' to the amputees and the born hand-less"
+                    label="Technology"
+                    path="/projectpage/Bionic_Hand/"
+                    funded= "100"/>
                     
 
                     <CardItem 
                     src={img4}
                     name="PuffPals"
-                    text="A wholesome life simulator game where you can collect, farm, customize and explore new lands with cute and fluffy PuffPals!" 
-                    label="Creative" 
-                    path="/projectpage"
-                    funded="100"/>
+                    text="A wholesome life simulator game where you can collect, farm, customize and explore new lands with cute and fluffy PuffPals!"
+                    label="Creative"
+                    path="/projectpage/PuffPals/"
+                    funded= "100"/>
 
                     <CardItem 
                     src={img5}
                     name="Mini Pupper"
-                    text="A mini robot puppy ready to be your companion!" 
-                    label="Technology" 
-                    path="/projectpage"
-                    funded="100"/>
+                    text="A mini robot puppy ready to be your companion!"
+                    label="Technology"
+                    path="/projectpage/Mini_Pupper"
+                    funded= "100"/>
                 </ul>
             </div>
-        </div>
-      </div>
+            </div>
+    </div>
   )
 }
 
